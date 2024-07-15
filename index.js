549d505af042899d3104024fdd06e0da34ef811c
ermanos <herman226.r@gmail.com>
2024-07-15 17:41:38 +0300

    add-comments

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import firestoreService from "./FirestoreService.js";
 async function initializeAndStartCronJobs() {
   await firestoreService.initializeCache();
 
-  cron.schedule("*/10 * * * *", () => firestoreService.updateWildfireData());
-  cron.schedule("0 0 * * *", () => firestoreService.deleteOldMarkers());
+  cron.schedule("*/10 * * * *", () => firestoreService.updateWildfireData()); // every 10 minutes
+  cron.schedule("0 0 * * *", () => firestoreService.deleteOldMarkers()); // once every day at 00:00
 
   // Initial run
   await firestoreService.updateWildfireData();
