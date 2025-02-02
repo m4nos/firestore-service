904e10c6c5a4df3939abbbc949d2ce7442f7b771
ermanos <herman226.r@gmail.com>
2025-02-02 14:42:33 +0200

    run-cleanup-every-hour

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import cron from "node-cron";
-import firestoreService from "./FirestoreService.js";
+import cron from 'node-cron';
+import firestoreService from './FirestoreService.js';
 
 async function initializeAndStartCronJobs() {
   await firestoreService.initializeCache();
 
-  cron.schedule("*/10 * * * *", () => firestoreService.updateWildfireData()); // every 10 minutes
-  cron.schedule("0 0 * * *", () => firestoreService.deleteOldMarkers()); // once every day at 00:00
+  cron.schedule('*/10 * * * *', () => firestoreService.updateWildfireData()); // every 10 minutes
+  cron.schedule('0 * * * *', () => firestoreService.deleteOldMarkers()); // Run once every hour at the start of the hour
 
   // Initial run
   await firestoreService.updateWildfireData();
